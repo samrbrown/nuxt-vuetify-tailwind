d8584d68b3de679be11e7e63a3f5166ec9e629c8
akerosambrown <sam.brown@akerolabs.com>
2024-01-24 12:05:36 +0000

    installed tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  },
   build: {
     transpile: ['vuetify'],
   },
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Hello world</h1>
+    <v-app-bar>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>App Bar</v-app-bar-title>
+    </v-app-bar>
+    <h1 class="font-bold">Hello world</h1>
+    <v-btn :rounded="'xl'" @click="overlay = !overlay">Click Here!</v-btn>
+    <v-overlay v-model="overlay"></v-overlay>
+    <v-text-field label="Label"></v-text-field>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const overlay = ref(null)
+</script>
